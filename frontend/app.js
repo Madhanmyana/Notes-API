2c0d4326b40fb3399a3dc8f132e8c3a52b1b8860
Madhanmyana <madhanmyana99@gmail.com>
2026-06-07 21:33:02 +0530

    updated api base url in app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://notes-api-mls1.onrender.com';
 
 // DOM Elements
 const notesListContainer = document.getElementById('notes-list-container');
